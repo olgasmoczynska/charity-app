3232196f6c8511e96393e1539a11b6c0b8d13202
olgasmoczynska <53126593+olgasmoczynska@users.noreply.github.com>
2020-10-19 18:50:33 +0200

    Add reset-css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.scss';
-import 'reset-css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
