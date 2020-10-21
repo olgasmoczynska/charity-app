6666ffd9d49c38108e15b2d6df74773e56ab1e62
olgasmoczynska <53126593+olgasmoczynska@users.noreply.github.com>
2020-10-21 17:16:12 +0200

    Create Home component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import HomeNav from './HomeNav';
+import HomeHeader from './HomeHeader';
+import HomeThreeColumns from './HomeThreeColumns';
+
+function Home() {
+    return (
+        <>
+        <HomeNav />
+        <HomeHeader />
+        {/* <HomeThreeColumns /> */}
+        </>
+    )
+}
+
+export default Home;
