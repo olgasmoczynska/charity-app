327042b92b4d1e150555e85b48aaa7fec7494b5e
olgasmoczynska <53126593+olgasmoczynska@users.noreply.github.com>
2020-10-22 23:05:43 +0200

    Add components of the homepage structure to be rendered by the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,13 +2,19 @@ import React, {useState, useEffect} from 'react';
 import HomeNav from './HomeNav';
 import HomeHeader from './HomeHeader';
 import HomeThreeColumns from './HomeThreeColumns';
+import HomeFourSteps from './HomeFourSteps';
+import HomeAboutUs from './HomeAboutUs';
+import HomeOrganizations from './HomeOrganizations';
 
 function Home() {
     return (
         <>
         <HomeNav />
         <HomeHeader />
-        {/* <HomeThreeColumns /> */}
+        <HomeThreeColumns />
+        <HomeFourSteps />
+        <HomeAboutUs />
+        <HomeOrganizations />
         </>
     )
 }
