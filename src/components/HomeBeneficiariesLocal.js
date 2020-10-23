65f27cdf482041ac35e091a98992fa713cfa47af
olgasmoczynska <53126593+olgasmoczynska@users.noreply.github.com>
2020-10-23 17:12:57 +0200

    Remove unnecessary imports for cleaner code

--- src/components/HomeBeneficiariesLocal.js
+++ src/components/HomeBeneficiariesLocal.js
@@ -4,25 +4,15 @@ import Beneficiary from './Beneficiary';
 
 function HomeBeneficiariesLocal () {
     const [items, setItems] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 3;
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
     useEffect(() => {
         getBeneficiaries()
         .then(data => setItems(data.local))
     }, []);
 
-    const handlePageChange = (pageNumber) => {
-        setCurrentPage(pageNumber)
-    };
-    
-    const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
-
     return (
         <div className="beneficiary-wrapper">
-        {currentItems.map( ( item, index ) => { return <Beneficiary key={index}
+        {items.map( ( item, index ) => { return <Beneficiary key={index}
                           name={item.name}
                           mission={item.mission}
                           requests={item.requests}
